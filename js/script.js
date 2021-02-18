46efcce07269836163758d776d7d983304085ba5
Karol Wisz <karolwiszniewski95@gmail.com>
2021-02-18 19:00:42 +0100

    finish generateTags function

--- js/script.js
+++ js/script.js
@@ -100,32 +100,32 @@ function generateTitleLinks(){
 generateTitleLinks();
 
 
-/* generateTags */
+/* generateTags function */
+
 function generateTags(){
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
   /* START LOOP: for every article: */
   for(let article of articles){
-  /* find tags wrapper */
-  const tagList = article.querySelector(optArticleTagsSelector);
-  /* make html variable with empty string */
-  tagList.innerHTML = '';
-  /* get tags from data-tags attribute */
-  const articleTags = article.getAttribute('data-tags');
-  console.log(articleTags);
-  /* split tags into array */
-  const articleTagsArray = articleTags.split(' ');
-  /* START LOOP: for each tag */
+    /* find tags wrapper */
+    const articleTag = article.querySelector(optArticleTagsSelector);
+    /* make html variable with empty string */
+    let html = '';
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+    /* split tags into array */
+    const articleTagsArray = articleTags.split('');
+    /* START LOOP: for each tag */
+    for(let tag of articleTagsArray){
+      /* generate HTML of the link */
+    const tagHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li> ';
+    /* add generated code to html variable */
+    html = html + tagHTML;
+    }
+    /* insert HTML of all the links into the tags wrapper */
 
-  /* generate HTML of the link */
+    articleTag.insertAdjacentHTML('afterbegin', html);
 
-  /* add generated code to html variable */
-
-  /* END LOOP: for each tag */
-
-  /* insert HTML of all the links into the tags wrapper */
-
-  /* END LOOP: for every article: */
+  }
 }
-
 generateTags();
